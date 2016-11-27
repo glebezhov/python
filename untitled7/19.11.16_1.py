49d4055adc250a634c3f57acfcb4547030327235
Gleb Ezhov <glebezhov@glebezhov-osx.Dlink>
2016-11-27 14:00:58 +0300

    fibonacci

--- untitled7/19.11.16_1.py
+++ untitled7/19.11.16_1.py
@@ -1,12 +1,11 @@
 n = int(input())
 s = 0
 i = 1
+x = 1
 while i <= n:
    k = i * 2 - 1
    k = 1 / (k ** 2)
+   x = x * -1
+   s = s + (x * k)
    i += 1
-   if i % 2 != 0:
-      s = s - k
-   else:
-      s = s + k
 print (s)
---
+++ untitled7/26.py
@@ -0,0 +1,17 @@
+n = int(input())
+a = b = 1
+i = 2
+s = 0
+while i >= n:
+    print (1)
+    break
+while i < n:
+    s = a + b
+    a = b
+    b = s
+    i += 1
+print (s)
+
+
+
+
